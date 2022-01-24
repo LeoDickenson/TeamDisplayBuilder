356e2cfb48a1fb5fbcf5a14972fe95d8a42dddc0
Leo Dickens <dickens.leo@gmail.com>
2022-01-24 06:11:56 -0600

    nearly completed functionality, need to reformat results and make it loop the menu

--- public/index.js
+++ public/index.js
@@ -1,20 +1,51 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const internTemplate = 
+const internTemplate = ({name, ID, email, github,}) =>
 `<div class="col-sm-6">
   <div class="card text-dark text-center bg-light mb-3" style="max-width: 18rem;">
-    <div class="card-header">Name</div>
+    <div class="card-header">${name}</div>
     <div class="card-body">
-        <h5 class="card-title">Role</h5>
+        <h5 class="card-title">INTERN</h5>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID</li>
-            <li class="list-group-item">Email</li>
-            <li class="list-group-item">Github</li>
+            <li class="list-group-item">${ID}</li>
+            <li class="list-group-item">${email}</li>
+            <li class="list-group-item">${github}</li>
         </p>
     </div>
   </div>
-</div>`
+</div>`;
+
+const engineerTemplate = ({name, ID, email, github,}) =>
+`<div class="col-sm-6">
+  <div class="card text-dark text-center bg-light mb-3" style="max-width: 18rem;">
+    <div class="card-header">${name}</div>
+    <div class="card-body">
+        <h5 class="card-title">ENGINEER</h5>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${ID}</li>
+            <li class="list-group-item">${email}</li>
+            <li class="list-group-item">${github}</li>
+        </p>
+    </div>
+  </div>
+</div>`;
+
+const projectManagerTemplate = ({name, ID, email, github,}) =>
+`<div class="col-sm-6">
+  <div class="card text-dark text-center bg-light mb-3" style="max-width: 18rem;">
+    <div class="card-header">${name}</div>
+    <div class="card-body">
+        <h5 class="card-title">PROJECT MANAGER</h5>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${ID}</li>
+            <li class="list-group-item">${email}</li>
+            <li class="list-group-item">${github}</li>
+        </p>
+    </div>
+  </div>
+</div>`;
+
 
 // const finishedTeam = fs.readFile('teamHTML.txt', 'utf8', (error, data) =>
 // error ? console.error(error) : console.log(data));
@@ -54,6 +85,7 @@ inquirer.prompt([
 
   if (userInput.menuChoice === 'add engineer')
    console.log('adding engineer');
+
   if (userInput.menuChoice === 'add intern')
   console.log('***ADDING AN INTERN***')
   inquirer.prompt([
@@ -77,7 +109,10 @@ inquirer.prompt([
     message: 'enter github username',
     name: 'github'
   }
-]).then((newMember) => {console.log(newMember)});
+]).then((answers) => {  fs.appendFile('index.html', internTemplate(answers), (err) =>
+err ? console.error(err) : console.log('Success!'))
+});
+
   if (userInput.menuChoice === 'finish building team')
   console.log('finishing team');
 
@@ -102,5 +137,6 @@ inquirer.prompt([
 
   // fs.appendFile('index.html', testHTML, (err) =>
   // err ? console.error(err) : console.log('Success!'))
+  
 
 });
