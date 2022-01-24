eea9b4269fae3c0d9334430deafac3635fa9c280
Leo Dickens <dickens.leo@gmail.com>
2022-01-24 04:05:27 -0600

    testing appending to html file functionality

--- public/index.js
+++ public/index.js
@@ -35,7 +35,23 @@ inquirer.prompt([
   console.log(userInput.teammateName)
   console.log(userInput.teammateEmail)
 
-  fs.appendFile('index.html', JSON.stringify(userInput), (err) =>
+  let testHTML = `    <div class="container">
+  <div class="col-sm-6">
+      <div class="card text-dark text-center bg-light mb-3" style="max-width: 18rem;">
+          <div class="card-header">Name</div>
+          <div class="card-body">
+              <h5 class="card-title">Role</h5>
+              <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Email</li>
+                  <li class="list-group-item">A second item</li>
+                  <li class="list-group-item">A third item</li>
+              </p>
+          </div>
+      </div>
+  </div>
+</div>`
+
+  fs.appendFile('index.html', testHTML, (err) =>
   err ? console.error(err) : console.log('Success!'))
 
 });
