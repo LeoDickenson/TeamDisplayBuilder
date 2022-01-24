89ba2d7abbf83818f17a90723c4acc52b2a9c882
Leo Dickens <dickens.leo@gmail.com>
2022-01-24 05:24:10 -0600

    working on functionality

--- public/index.js
+++ public/index.js
@@ -3,9 +3,18 @@ const fs = require('fs');
 
 // NEED A CONSTRUCTOR THAT MAKES AN HTML CARD ELEMENT AND USES THE USER INPUT TO FILL CARD ELEMENT WITH TEAM MEMBER INFO
 // HTML DOC NEEDS TO DISPLAY ALL ITEMS IN A FLEX ROW
-function TeammateMaker(name, email) {
+// prompt for input with 4 options "add project managaer" "add engineer" "add Intern" "finish building team"
+// finish building team creates the html
+// add manager uses constructor to build manager object and stores string (html formatting) into a .txt fil
+//add engineer uses constructor to build engineer object
+//add intern uses constructor to build intern object
+// finish building team reads the .txt file and then creates an html file from the .txt file data
+function TeammateMaker(role, name, email, github) {
+  this.role = role;
   this.name = name;
   this.email = email;
+  this.github = github;
+
   // this.makeNoise = () => {
   //   if (this.raining === true) {
   //     console.log(this.noise);
@@ -15,16 +24,11 @@ function TeammateMaker(name, email) {
 
 inquirer.prompt([
   {
-    type: 'input',
-    message: 'What is your name?',
-    name: 'teammateName',
-  },
-  {
-    type: 'input',
-    message: 'What is your email?',
-    name: 'teammateEmail',
-  },
-])
+    type: 'list',
+    message: 'Please pick an option',
+    name: 'menuChoice',
+    choices: ['add engineer','add intern','finish building team'] 
+  }])
 
 
 
@@ -32,26 +36,39 @@ inquirer.prompt([
 
 .then((userInput) => { 
 
-  console.log(userInput.teammateName)
-  console.log(userInput.teammateEmail)
+  if (userInput.menuChoice === 'add engineer')
+   console.log('addin engineer');
+  if (userInput.menuChoice === 'add intern')
+  console.log('adding intern')
+  inquirer.prompt([
+  {
+    type: 'input',
+    message: 'enter name',
+    name: 'name'
+  }]).then((newMember) => {console.log(newMember)});
+  if (userInput.menuChoice === 'finish building team')
+  console.log('finishing team');
 
-  let testHTML = `    <div class="container">
-  <div class="col-sm-6">
-      <div class="card text-dark text-center bg-light mb-3" style="max-width: 18rem;">
-          <div class="card-header">Name</div>
-          <div class="card-body">
-              <h5 class="card-title">Role</h5>
-              <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Email</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
-              </p>
-          </div>
-      </div>
-  </div>
-</div>`
+//   console.log(userInput.teammateName)
+//   console.log(userInput.teammateEmail)
 
-  fs.appendFile('index.html', testHTML, (err) =>
-  err ? console.error(err) : console.log('Success!'))
+//   let testHTML = `    <div class="container">
+//   <div class="col-sm-6">
+//       <div class="card text-dark text-center bg-light mb-3" style="max-width: 18rem;">
+//           <div class="card-header">Name</div>
+//           <div class="card-body">
+//               <h5 class="card-title">Role</h5>
+//               <ul class="list-group list-group-flush">
+//                   <li class="list-group-item">Email</li>
+//                   <li class="list-group-item">A second item</li>
+//                   <li class="list-group-item">A third item</li>
+//               </p>
+//           </div>
+//       </div>
+//   </div>
+// </div>`
+
+//   fs.appendFile('index.html', testHTML, (err) =>
+//   err ? console.error(err) : console.log('Success!'))
 
 });
